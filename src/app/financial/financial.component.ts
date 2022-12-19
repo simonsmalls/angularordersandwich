285ef71095a30d29ada22fa55563c443387e2d4d
esbsi <esben.six@protonmail.com>
2022-12-19 12:14:58 +0100

    noscript

--- src/app/financial/financial.component.ts
+++ src/app/financial/financial.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {async, Observable} from "rxjs";
 import {FinanceDto} from "../models/finance-dto";
 import {parse} from "@angular/compiler-cli/linker/babel/src/babel_core";
+import {Shop} from "../models/shop";
 
 @Component({
   selector: 'app-financial',
@@ -13,33 +14,52 @@ export class FinancialComponent implements OnInit{
 
   yearMonth: string;
   financeDto: FinanceDto;
-  shops: Observable<{ id: number; name: string }[]>;
-  summary: string = "";
-  averagePrice;
-  averageShopPrice;
+//  shopList: Observable<{ id: number; name: string }[]>;
 
-  setSelectedShop(id: number){
-    this.financeDto.shop = id;
+//  shops: Shop[];
+//  selectedShop: Shop;
+  selectedShop: number;
+  summary: string;
+  averagePrice: number;
+  averageShopPrice;
+  shops: Observable<Shop[]>;
+
+  setSelectedShop(){
   }
 
   setYearMonth(yyyyxMM: string){
-    let year: number = parseInt(yyyyxMM[0-3]);
-    let month: number = parseInt(yyyyxMM[5-6]);
+    let year: number = parseInt(yyyyxMM.substring(0, 4));
+    console.log(year);
+    let month: number = parseInt(yyyyxMM.substring(5, 7));
+    console.log(month);
     this.financeDto.year = year;
+    console.log(this.financeDto.year);
     this.financeDto.month = month;
+    console.log(this.financeDto.month);
+
   }
 
   financialSummary(){
+    this.financeDto.shop = this.selectedShop; //.id;
+    console.log(this.financeDto.shop);
+    console.log(this.yearMonth);
+    console.log(typeof this.yearMonth);
     this.setYearMonth(this.yearMonth);
-    this.averagePrice = this.httpClient.post<number>('http://localhost:8080/ordersandwich/api/finance/monthlyprice', this.financeDto);
-    this.summary = 'Average price' + this.averagePrice;
+    this.httpClient.post<number>('http://localhost:8080/ordersandwich/api/finance/monthlyprice', this.financeDto)
+      .subscribe((c)=> this.averagePrice = c);
+    console.log(this.averagePrice);
+    this.summary = 'Total price of all orders at all shops this month: ' + this.averagePrice;
+    // 'Average price of a sandwich this month at all shops: '
+    console.log(this.summary);
   }
 
   constructor(private httpClient: HttpClient) {
   }
 
   ngOnInit(){
-    this.shops = this.httpClient.get<{id: number, name: string}[]>('http://localhost:8080/ordersandwich/api/shop');
+ //   this.shops = this.httpClient.get<{id: number, name: string}[]>('http://localhost:8080/ordersandwich/api/shop');
+//    this.httpClient.get<Shop[]>('http://localhost:8080/ordersandwich/api/shop').subscribe((c) => this.shops = c);
+    this.shops = this.httpClient.get<Shop[]>('http://localhost:8080/ordersandwich/api/shop');
     this.financeDto = new FinanceDto();
   }
 
--- src/app/service/order-today.service.ts
+++ src/app/service/order-today.service.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {Session} from "../models/session";
 import {Ordertoday} from "../models/ordertoday";
 import {Person} from "../models/person";
+import {SandwichOrder} from "../models/sandwich-order";
 
 @Injectable({
   providedIn: 'root'
@@ -15,19 +16,31 @@ export class OrderTodayService {
 
   constructor(
     protected http: HttpClient,
-  )
-  {
-
-
-  }
+  ) {}
 
   getOrderToday(): Observable<Ordertoday>
   {
-
     return this.http.get<Ordertoday>(this.url );
   }
 
   noOrder(person:Person){
     return this.http.post(this.url+"/none", person );
   }
+
+  order(sandwichOrder: SandwichOrder){
+    return this.http.post(this.url, sandwichOrder);
+  }
+
+  newOrderTomorrow(id: number){
+    return this.http.post(this.url + "new/tomorrow/" + id, null);
+  }
+
+  newOrderToday(id: number){
+    return this.http.post(this.url + "new/today/" + id, null);
+  }
+
+  check(person: Person){
+    return this.http.post<SandwichOrder>(this.url + "check/person", person);
+  }
+
 }
