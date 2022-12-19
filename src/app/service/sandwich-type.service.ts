ba9ddb1034067c6a62890195430e9d30920f27c9
esbsi <esben.six@protonmail.com>
2022-12-19 16:53:05 +0100

    noscript

--- src/app/service/sandwich-type.service.ts
+++ src/app/service/sandwich-type.service.ts
@@ -8,13 +8,46 @@ import {SandwichType} from "../models/sandwichType";
   providedIn: 'root'
 })
 export class SandwichTypeService {
-  url:string='http://localhost:8080/ordersandwich/api/sandwich'
+
+  url:string='http://localhost:8080/ordersandwich/api/sandwich/'
 
   constructor(   protected http: HttpClient,) { }
 
-  getSandwichTypes(id:number): Observable<Array<SandwichType>>
-  {
 
-    return this.http.get<Array<SandwichType>>(this.url + '/shop/'+ id  );
+  getSandwichTypes(id:number): Observable<Array<SandwichType>> {
+    return this.http.get<Array<SandwichType>>(this.url + 'shop/'+ id  );
   }
+
+
+
+
 }
+/* TODO
+@GetMapping("{id}")
+public ResponseEntity<?> findSandwichTypeById(@PathVariable int id) throws SandwichTypeNotFoundException {
+  SandwichType sandwichType = sandwichTypeService.findSandwichTypeById(id);
+  return new ResponseEntity<>(sandwichType, HttpStatus.OK);
+}
+
+@GetMapping("query")
+public ResponseEntity<?> findSandwichType(@RequestParam("name") String name, @RequestParam("shopid") int shopId) throws SandwichTypeNotFoundException, ShopNotFoundException {
+  SandwichType sandwichType = sandwichTypeService.findSandwichType(name, shopId);
+  return new ResponseEntity<>(sandwichType, HttpStatus.OK);
+}
+
+@DeleteMapping("{id}")
+public void removeSandwichType(@PathVariable int id) throws SandwichTypeNotFoundException {
+  sandwichTypeService.removeSandwichType(id);
+}
+
+@PostMapping("")
+public void addSandwichType(@RequestBody SandwichType sandwichType) throws SandwichTypeAlreadyExistsException {
+  sandwichTypeService.addSandwichType(sandwichType);
+}
+
+@GetMapping("")
+public ResponseEntity<?> getSandwichTypes() {
+  List<SandwichType> sandwichTypes = sandwichTypeService.getSandwichTypes();
+  return new ResponseEntity<>(sandwichTypes, HttpStatus.OK);
+}
+*/
