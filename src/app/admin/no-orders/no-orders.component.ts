78d0451d1d6a9e1affb9fa8a0fe9497af1649299
Simon Hazevoets <simonsmalls568@gmail.com>
2022-12-20 14:21:48 +0100

    fsdf

--- src/app/admin/no-orders/no-orders.component.ts
+++ src/app/admin/no-orders/no-orders.component.ts
@@ -1,14 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {OrderTodayService} from "../../service/order-today.service";
 import {Person} from "../../models/person";
+import {SandwichType} from "../../models/sandwichType";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-no-orders',
   templateUrl: './no-orders.component.html',
   styleUrls: ['./no-orders.component.css']
 })
+
 export class NoOrdersComponent implements OnInit{
   persons:Array<Person>=[];
+  dataSource = new MatTableDataSource<Person>();
+  displayedColumns: string[] = ['firstName','lastName'];
 
   constructor(private os:OrderTodayService) {
   }
@@ -16,7 +21,7 @@ export class NoOrdersComponent implements OnInit{
 
      this.os.checkallorderPersons().subscribe((c)=>{
        this.persons=c;
-
+       this.dataSource.data=c
      })
   }
 
